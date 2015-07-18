5bdd4a1bc7df82e7c3fcb2bb5aa5af12fd6f830d
Shown Tien <hightian@gmail.com>
2015-07-18 22:55:35 +0800

    Proxy UI finished

--- client/app/proxy/proxy.controller.js
+++ client/app/proxy/proxy.controller.js
@@ -3,7 +3,12 @@
 angular.module('proxyManagerApp')
   .controller('ProxyCtrl', function($scope, $http, Modal, Proxy, Util, $interval, $timeout) {
     $scope.refresh = function() {
-      $scope.proxys = Proxy.query();
+      $scope.refresh_status = true;
+      $scope.proxys = Proxy.query(function() {
+        $timeout(function() {
+        $scope.refresh_status = false;
+        }, 2000);
+      });
     };
     // $scope.animationsEnabled = true;
     var saveProxy = function(proxy) {
@@ -25,10 +30,39 @@ angular.module('proxyManagerApp')
 
     $scope.refresh();
 
-    $scope.clickBtn = function() {
-      $scope.loading = true; // start loading
+    $scope.start_or_stop_proxy = function(proxy) {
+      proxy.loading = true; // start loading
       $timeout(function() {
-        $scope.loading = false; // stop loading
+        if (proxy.status === true) {
+          stop_proxy(proxy);
+        } else {
+          start_proxy(proxy);
+        }
+        proxy.loading = false; // stop loading
       }, 2000);
     }
+
+    $scope.proxy_status = function(proxy) {
+      if (proxy.status === true) {
+        return "Running";
+      } else {
+        return "Not Running"
+      }
+    };
+
+    $scope.status_button_class = function (proxy) {
+      if (proxy.status === true) {
+        return "btn btn-success ladda-button";
+      } else {
+        return "btn btn-danger  ladda-button";
+      }
+    };
+
+    var start_proxy = function (proxy) {
+      console.log('start proxy');
+    };
+
+    var stop_proxy = function (proxy) {
+      console.log('stop proxy');
+    };
   });
