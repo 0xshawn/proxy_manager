8bec237cc5f8653341c971b6f024fe6d94a58529
Shown Tien <hightian@gmail.com>
2015-07-18 15:22:37 +0800

    add bower.json overrides

--- client/app/app.js
+++ client/app/app.js
@@ -1,16 +1,21 @@
 'use strict';
 
 angular.module('proxyManagerApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ui.router',
-  'ui.bootstrap',
-  'ladda'
-])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ui.router',
+    'ui.bootstrap',
+    'angular-ladda'
+  ])
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+  })
+  .config(function(laddaProvider) {
+    laddaProvider.setOption({
+      style: 'expand-left'
+    });
   });
--- client/app/proxy/proxy.controller.js
+++ client/app/proxy/proxy.controller.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('proxyManagerApp')
-  .controller('ProxyCtrl', function ($scope, $http, Modal, Proxy, Util, $interval, $timeout) {
-    $scope.refresh = function () {
+  .controller('ProxyCtrl', function($scope, $http, Modal, Proxy, Util, $interval, $timeout) {
+    $scope.refresh = function() {
       $scope.proxys = Proxy.query();
     };
     // $scope.animationsEnabled = true;
-    var saveProxy = function (proxy) {
+    var saveProxy = function(proxy) {
       if (!('owner' in proxy)) {
         proxy['owner'] = Util.randomString(10);
       }
@@ -15,43 +15,20 @@ angular.module('proxyManagerApp')
       $scope.refresh();
     };
 
-    $scope.newProxy = function () {
+    $scope.newProxy = function() {
       Modal.new.proxy(saveProxy)();
     };
 
-    $scope.start_or_stop = function () {
+    $scope.start_or_stop = function() {
       console.log('button click');
     };
 
     $scope.refresh();
 
-
-
-    $scope.showLoading = function () {
-      // Set ladda loading true
-      // Loading spinner will be shown;
-      $scope.laddaLoading = true;
-      $timeout(function () {
-        // Set ladda loading false after 3 Seconds.
-        // Loading spinner will be hidden;
-        $scope.laddaLoading = false;
-      }, 3000);
-    };
-
-    // Example without progress Bar
-    $scope.loadingWithProgress = function () {
-      // Set progress 0;
-      $scope.laddaLoadingBar = 0;
-      // Run in every 30 milliseconds
-      var interval = $interval(function () {
-        // Increment by 1;
-        $scope.laddaLoadingBar++;
-        if ($scope.laddaLoadingBar >= 100) {
-          // Cancel interval if progress is 100
-          $interval.cancel(interval);
-          //Set ladda loading false
-          $scope.laddaLoadingBar = false;
-        }
-      }, 30);
-    };
+    $scope.clickBtn = function() {
+      $scope.loading = true; // start loading
+      $timeout(function() {
+        $scope.loading = false; // stop loading
+      }, 2000);
+    }
   });
